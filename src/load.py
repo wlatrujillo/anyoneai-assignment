be8fb5e813c3d13c4e32e3fcedd519f7093b21df
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2026-02-26 20:54:25 -0500

    fix: load

--- src/load.py
+++ src/load.py
@@ -11,10 +11,16 @@ def load(data_frames: Dict[str, DataFrame], database: Engine):
         data_frames (Dict[str, DataFrame]): A dictionary with keys as the table names
         and values as the dataframes.
     """
-    for table_name, dataframe in data_frames.items():
-        dataframe.to_sql(
-            name=table_name,
-            con=database,
-            if_exists="replace",
-            index=False,
-        )
+    dbapi_connection = database.raw_connection()
+    try:
+        for table_name, dataframe in data_frames.items():
+            print(f"Loading {table_name} into the database...")
+            dataframe.to_sql(
+                name=table_name,
+                con=dbapi_connection,
+                if_exists="replace",
+                index=False,
+            )
+        dbapi_connection.commit()
+    finally:
+        dbapi_connection.close()
