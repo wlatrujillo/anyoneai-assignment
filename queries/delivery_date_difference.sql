2d6d9f174bc5bda3dad890f5b4bd945476d260c8
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2026-02-27 19:18:34 -0500

    feat: add queries

--- queries/delivery_date_difference.sql
+++ queries/delivery_date_difference.sql
@@ -9,3 +9,18 @@
 -- 2. You can use the CAST function to convert a number to an integer.
 -- 3. You can use the STRFTIME function to convert a order_delivered_customer_date to a string removing hours, minutes and seconds.
 -- 4. order_status == 'delivered' AND order_delivered_customer_date IS NOT NULL
+
+select 
+    oc.customer_state as State, 
+    cast(
+        avg(
+            julianday(STRFTIME('%Y-%m-%d', oo.order_estimated_delivery_date)) - 
+            julianday(STRFTIME('%Y-%m-%d', oo.order_delivered_customer_date))
+        ) as integer
+    ) as Delivery_Difference
+from olist_customers oc 
+inner join olist_orders oo 
+    on oc.customer_id = oo.customer_id 
+where oo.order_status = 'delivered'
+    and oo.order_delivered_customer_date IS NOT NULL
+group by oc.customer_state;
--- queries/global_amount_order_status.sql
+++ queries/global_amount_order_status.sql
@@ -2,3 +2,10 @@
 -- This query will return a table with two columns: order_status and Amount. 
 -- The first one will have the different order status classes 
 -- and the second one the total amount of each.
+
+
+select 
+    oo.order_status as order_status, 
+    count(1) as Amount
+from olist_orders oo 
+    group by oo.order_status; 
