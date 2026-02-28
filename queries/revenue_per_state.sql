0593778515087fba48e9d273e817416b806a529d
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2026-02-27 23:04:35 -0500

    add: queries

--- queries/revenue_per_state.sql
+++ queries/revenue_per_state.sql
@@ -5,3 +5,17 @@
 
 -- HINT: 
 -- All orders should have a delivered status and the actual delivery date should be not null. 
+
+select 
+    oc.customer_state as customer_state, 
+    sum(oop.payment_value) as Revenue 
+from olist_orders oo 
+    inner join olist_customers oc 
+    on oo.customer_id = oc.customer_id 
+    inner join olist_order_payments oop 
+    on oo.order_id = oop.order_id 
+where oo.order_status = 'delivered'
+    and oo.order_delivered_customer_date is not null
+group by oc.customer_state
+order by Revenue desc
+limit 10;
